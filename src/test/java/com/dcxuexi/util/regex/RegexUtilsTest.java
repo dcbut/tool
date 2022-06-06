9265e2ceb6e0c21d87a635600bef6653087ce9d7
Dchuang <dongchuang@dc1g.com>
2022-06-06 19:35:38 +0800

    RegexUtils

---
+++ src/test/java/com/dcxuexi/util/regex/RegexUtilsTest.java
@@ -0,0 +1,15 @@
+package com.dcxuexi.util.regex;
+
+public class RegexUtilsTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("验证手机号: "+RegexUtils.isMobileSimple("15201830000"));
+
+
+
+    }
+
+
+
+}
