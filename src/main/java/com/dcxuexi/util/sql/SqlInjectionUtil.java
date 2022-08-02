55088a71de1466bdbd2e54936c55780952f6476b
Dchuang <dongchuang@dc1g.com>
2022-08-02 20:17:00 +0800

    SQL injection

--- src/main/java/com/dcxuexi/util/sql/SqlInjectionUtil.java
+++ src/main/java/com/dcxuexi/util/sql/SqlInjectionUtil.java
@@ -3,4 +3,9 @@ package com.dcxuexi.util.sql;
 public class SqlInjectionUtil {
     private String  SQL_STRING_TYPE = "STRING";
     private String  SQL_NUMBER_TYPE = "NUMBER";
+
+
+    public static void main(String[] args) {
+        System.out.println("--------");
+    }
 }
