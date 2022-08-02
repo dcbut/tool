3ffc36ea2769901589e6b75f2ebbd49ba32e5f33
Dchuang <dongchuang@dc1g.com>
2022-08-02 20:14:08 +0800

    pom

---
+++ src/main/java/com/dcxuexi/util/sql/SqlInjectionUtil.java
@@ -0,0 +1,6 @@
+package com.dcxuexi.util.sql;
+
+public class SqlInjectionUtil {
+    private String  SQL_STRING_TYPE = "STRING";
+    private String  SQL_NUMBER_TYPE = "NUMBER";
+}
--- src/test/java/com/dcxuexi/util/regex/RegexUtilsTest.java
+++ src/test/java/com/dcxuexi/util/regex/RegexUtilsTest.java
@@ -4,12 +4,11 @@ public class RegexUtilsTest {
 
     public static void main(String[] args) {
 
-        System.out.println("验证手机号: "+RegexUtils.isMobileSimple("15201830000"));
-
-
+        System.out.println("验证手机号: " + RegexUtils.isMobileSimple("15201830000"));
+        System.out.println("验证手机号: " + RegexUtils.isMobileSimple("15201830001"));
+        System.out.println("验证手机号: " + RegexUtils.isMobileSimple("15201830002"));
 
     }
-
 
 
 }
